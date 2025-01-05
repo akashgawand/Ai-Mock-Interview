231d9b552ddb0a45bd4be64fc885d91bff27824d
Akash <120485931+akashgawand@users.noreply.github.com>
2025-01-05 19:57:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-// import { Toaster } from "@/components/ui/sonner";
 import { Toaster } from "sonner";
 
 const geistSans = localFont({
@@ -23,14 +22,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Toaster/>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Toaster />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
